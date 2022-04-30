808dddf3cbe571b31d4af841f1d5e9828b83582e
Shahimat <ngs22071993@gmail.com>
2022-04-30 14:30:26 +0300

    build[server]: added .env PORT

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const open = require('open');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static('dist'));
