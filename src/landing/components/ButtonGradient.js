874c2740dd14502dc29a5fb25b9a01342c295899
Shahimat <ngs22071993@gmail.com>
2022-05-01 04:00:56 +0300

    refactor[components]: ButtonGradient

--- src/landing/components/ButtonGradient.js
+++ src/landing/components/ButtonGradient.js
@@ -1,13 +1,13 @@
 module.exports = (lf, slf, project) => {
-  const { $print: $p, $attr, a, span, className, href } = lf;
+  const { $print: $p, $attr, button, span, className, href } = lf;
 
-  return (text, classButtonGradient = '', sHref = '', sTarget = '') => {
-    return a (
+  return (text, classButtonGradient = '', sHref = '') => {
+    return button (
       span(text, className('base_button_gradient_text')),
       $p(
         className($p('base_button_gradient ', classButtonGradient)),
+        $attr('onclick', `location.href='${sHref}';`),
         href(sHref),
-        $attr('target', sTarget)
       )
     );
   };
