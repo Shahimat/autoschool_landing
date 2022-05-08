983d1afe3e890cd3a33517c2de07d8ed4e5203a3
Shahimat <ngs22071993@gmail.com>
2022-05-08 14:09:32 +0300

    refactor[Section]: with config

--- src/landing/components/Section.js
+++ src/landing/components/Section.js
@@ -1,10 +1,27 @@
 module.exports = (lf, slf, project) => {
-  const { $print: $p, section, className } = lf;
+  const { $print: $p, section, className, id } = lf;
 
-  return (sClass, ...args) => {
-    return section(
-      $p(...args),
-      className(sClass)
-    );
+  return (input, ...args) => {
+    if (typeof(input) === 'string') {
+      return section(
+        $p(...args),
+        className(input)
+      );
+    } else if (typeof(input) === 'object') {
+      const config = {
+        sClass: '',
+        id: '',
+      }
+      Object.assign(config, input);
+      return section(
+        $p(...args),
+        $p(
+          config.sClass? className(config.sClass): '',
+          config.id? id(config.id): ''
+        )
+      );
+    } else {
+      throw new Error(`Section: expected class <string> or <object> but found "${input}"`);
+    }
   };
 }
