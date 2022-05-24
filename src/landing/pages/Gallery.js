8ada2cdcee7363045dbe46d9bc608713fcdbb679
Shahimat <ngs22071993@gmail.com>
2022-05-25 01:51:07 +0300

    feat[GalleryPage]: adaptation

--- src/landing/pages/Gallery.js
+++ src/landing/pages/Gallery.js
@@ -1,21 +1,12 @@
 module.exports = (lf, slf, project) => {
 
   const { $print: $p, $attr, doctype, html, head, body, main, meta, title, link, script, src, charset, className, 
-    div, a, href, img, alt } = lf;
+    div, a, href, img, alt, section } = lf;
   const Header = project.def('Header');
   const Footer = project.def('Footer');
+  const Wrapper = project.def('Wrapper');
   const H1 = project.def('H1');
   const Model = project.model('photoModel');
-
-  const Container = (...args) => div(
-    div(
-      $p(
-        ...args
-      ),
-      className('gallery_page--content_wrapper')
-    ),
-    className('gallery_page--content')
-  );
 
   return () => {
 
@@ -56,26 +47,29 @@ module.exports = (lf, slf, project) => {
           body(
             $p(
               Header(),
-              Container(
-                H1('Фото автошколы МИК 2000'),
-                ...Model.get().map(elem => a(
-                  img(
-                    null,
+              section(
+                Wrapper(
+                  H1('Фото автошколы МИК 2000'),
+                  ...Model.get().map(elem => a(
+                    img(
+                      null,
+                      $p(
+                        className('gallery_photo_img'),
+                        src(`assets/photos/${elem}`),
+                        alt(elem)
+                      )
+                    ),
                     $p(
-                      className('gallery_photo_img'),
-                      src(`assets/photos/${elem}`),
-                      alt(elem)
+                      className('gallery_photo'),
+                      href(`assets/photos/${elem}`),
+                      $attr('data-fancybox', 'gallery'),
+                      $attr('data-caption', elem),
+                      $attr('data-width', '640'),
+                      $attr('data-height', '480'),
                     )
-                  ),
-                  $p(
-                    className('gallery_photo'),
-                    href(`assets/photos/${elem}`),
-                    $attr('data-fancybox', 'gallery'),
-                    $attr('data-caption', elem),
-                    $attr('data-width', '640'),
-                    $attr('data-height', '480'),
-                  )
-                ))
+                  ))
+                ),
+                className('gallery_page--content')
               ),
               Footer(),
               script(null, src('https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js'))
