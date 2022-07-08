be0741d9025e1e18ad1bf77081fdf0890221282c
Shahimat <ngs22071993@gmail.com>
2022-07-09 00:48:31 +0300

    build[logic]: js -> ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 
 let config = {
   mode: 'development',
-  entry: path.join(__dirname, 'src', 'logic', 'index.js'),
+  entry: path.join(__dirname, 'src', 'logic', 'index.ts'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
 
