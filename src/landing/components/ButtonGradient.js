11b06ac15072c33031f97e9dfa0b483dd393d6be
Shahimat <ngs22071993@gmail.com>
2022-07-09 00:17:20 +0300

    fix[components]: remove href from ButtonGradient

--- src/landing/components/ButtonGradient.js
+++ src/landing/components/ButtonGradient.js
@@ -1,13 +1,11 @@
 module.exports = (lf, slf, project) => {
   const { $print: $p, $attr, button, span, className, href } = lf;
 
-  return (text, classButtonGradient = '', sHref = '') => {
+  return (text, classButtonGradient = '') => {
     return button (
       span(text, className('base_button_gradient_text')),
       $p(
         className($p('base_button_gradient ', classButtonGradient)),
-        $attr('onclick', `location.href='${sHref}';`),
-        href(sHref),
       )
     );
   };
