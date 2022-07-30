4a332397d1f417196a35f17aa0ca20a557aebe00
Shahimat <ngs22071993@gmail.com>
2022-07-30 14:42:59 +0300

    refactor[bindings]: clear console.log

--- src/logic/model/bindings/test.js
+++ src/logic/model/bindings/test.js
@@ -3,10 +3,5 @@ export default {
   fields: [
     'is_open_modal'
   ],
-  callback: (element, field, value, instance) => {
-    console.log('Hello World!');
-    console.log(element);
-    console.log(field, value);
-    console.log(instance);
-  }
+  callback: (element, field, value, instance) => {}
 }
